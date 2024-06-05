209cff070286829be33fd1d17546b092c58baf51
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-06-05 16:37:46 -0300

    Update Resposive tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html', './src/*.css'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'col-B': 'hsl(223, 87%, 63%)',
+        'col-PB': 'hsl(223, 100%, 88%)',
+        'col-LR': 'hsl(354, 100%, 66%)',
+        'col-G': 'hsl(0, 0%, 59%)',
+        'col-VDB': 'hsl(209, 33%, 12%)',
+        'hover-btn': 'rgba(113,149,245,255)',
+        'hover-icon': 'rgba(75,121,248,255)'
+      },
+      fontFamily: {
+        'LibreFranklin': 'Libre Franklin'
+      }
+    },
   },
   plugins: [],
 }
